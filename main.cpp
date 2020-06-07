9ff90ad8da2d7126bd3480e556f5a8cf6c471f52
zendurix <mich111222333@gmail.com>
2020-06-07 23:49:17 +0200

    openGL linked

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
-#include <iostream>
+#include <GL/glut.h>
 
-int main()
+
+void render(void)
 {
-	std::cout<<"siema z linuksa";
-	std::cin.get();
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glMatrixMode(GL_MODELVIEW);
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS);
+
+
+
+  glLoadIdentity();
+  glRotatef(90, 0, 0, 1);
+  glScalef(1.0, 1.0, 1.0);
+  glTranslatef(-10000, -10000, 300);
+
+
+  glFlush();
+  glutSwapBuffers();
+}
+
+int main(int argc, char **argv)
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutCreateWindow("OPENGL");
+  glutInitWindowSize(1920, 1080);
+  glutDisplayFunc(render);
+  glutMainLoop();
+  return 0;
 }
